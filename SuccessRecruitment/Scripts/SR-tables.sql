b76ab0ac69050de083e4ca42b7d10c81fd42fc31
Adhiraj Kinlekar <adhirajkinlekar@outlook.com>
2021-08-26 23:03:48 +0530

    Added Script

--- SuccessRecruitment/Scripts/SR-tables.sql
+++ SuccessRecruitment/Scripts/SR-tables.sql
@@ -13,11 +13,14 @@ create table tblLogin (
 create table tblPages (
 	pageId int Primary key identity(1,1),
 	pageName varchar(20) not null,
-	parentPageId int null unique foreign key references tblPages(pageId) ,
+	parentPageId int null foreign key references tblPages(pageId),
 	createdBy uniqueidentifier not null,
 	createdDate datetime not null,
 	modifiedBy uniqueidentifier,
 	modifiedDate datetime,
+	isTab bit not null,
+	isAddEditPage bit not null,
+	isExternal bit not null,
 	isArchived bit default 0 not null
 )
 
@@ -30,4 +33,15 @@ create table tblUserPages (
 	modifiedBy uniqueidentifier,
 	modifiedDate datetime,
 	isArchived bit default 0 not null
+)
+create table tblRolePages (
+	rolePageId int primary key identity(1,1),
+	roleId int not null foreign key references tblRoles(roleId),
+	pageId int not null foreign key references tblPages(pageId),
+	createdBy uniqueidentifier not null,
+	createdDate datetime not null,
+	modifiedBy uniqueidentifier,
+	modifiedDate datetime,
+	isArchived bit default 0 not null,
+	CONSTRAINT UC_RolePage UNIQUE (roleId,pageId)
 )
